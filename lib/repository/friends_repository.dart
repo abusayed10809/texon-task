1ad528eaf00bfd1343fc1eef6c867c8671523ca3
unknown <abusayed10809@gmail.com>
2023-08-12 15:25:45 +0600

    creating model and repository testing

---
+++ lib/repository/friends_repository.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:friends_app/api_services/api_service.dart';
+import 'package:friends_app/model/friends_list_model.dart';
+import 'package:friends_app/utils/api_constants.dart';
+
+class FriendsRepository {
+  final ApiService apiService = ApiService();
+
+  Future<Either<String, List<FriendModel>>> fetchPostList({required int page}) async {
+    try {
+      dynamic response = await apiService.getResponse(url: '${ApiConstants.baseUrl}/?results=10&page=$page');
+      FriendsListModel friendsListModel = FriendsListModel.fromJson(response);
+      return Right(friendsListModel.results ?? []);
+    } catch (error) {
+      return Left(error.toString());
+    }
+  }
+}
--- test/api_test.dart
+++ test/api_test.dart
@@ -5,18 +5,36 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:friends_app/api_services/api_service.dart';
 
 import 'package:friends_app/main.dart';
+import 'package:friends_app/model/friends_list_model.dart';
+import 'package:friends_app/repository/friends_repository.dart';
 
 void main() {
-  test('api test', () async {
+  test('service test', () async {
     ApiService apiService = ApiService();
 
     final response = await apiService.getResponse(url: 'https://randomuser.me/api/?results=10');
 
     print(response.toString());
+  });
+
+  test('repository test', () async {
+    FriendsRepository friendsRepository = FriendsRepository();
+
+    final Either<String, List<FriendModel>> either = await friendsRepository.fetchPostList(page: 1);
+
+    either.fold(
+      (l) {
+        print('repository error : $l');
+      },
+      (r) {
+        print('repository success : $r');
+      },
+    );
   });
 }
