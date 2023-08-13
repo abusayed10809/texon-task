4ab35abf120d7dcd41bdf4e649c64f9a4fe9d01c
unknown <abusayed10809@gmail.com>
2023-08-13 16:01:25 +0600

    email feature complete

--- lib/view/friend_details/friend_details_screen.dart
+++ lib/view/friend_details/friend_details_screen.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:friends_app/controller/friend_controller.dart';
 import 'package:friends_app/model/friends_list_model.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class FriendDetailsScreen extends GetView<FriendController> {
   const FriendDetailsScreen({Key? key}) : super(key: key);
@@ -10,103 +12,228 @@ class FriendDetailsScreen extends GetView<FriendController> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          width: Get.width,
-          height: Get.height,
-          padding: EdgeInsets.symmetric(
-            horizontal: Get.width * 0.025,
-            vertical: Get.height * 0.02,
-          ),
-          child: Obx(
-            () {
-              FriendModel friendModel = controller.friendModel.value;
-              return Column(
-                children: [
-                  ///
-                  /// image
-                  Image.network(
-                    friendModel.picture!.large ?? '',
-                    width: Get.width,
-                    height: Get.height * 0.6,
-                    fit: BoxFit.cover,
-                  ),
-
-                  SizedBox(
-                    height: Get.height * 0.01,
-                  ),
-
-                  ///
-                  /// address
-                  Text(
-                    ('${friendModel.location?.city}, ' ?? '') + ('${friendModel.location?.state}, ' ?? '') + ('${friendModel.location?.country}, ' ?? ''),
-                    style: TextStyle(
-                      fontSize: Get.textScaleFactor * 20,
-                    ),
-                  ),
-
-                  SizedBox(
-                    height: Get.height * 0.02,
-                  ),
-
-                  ///
-                  /// email
-                  Container(
-                    color: Colors.grey[400],
-                    width: Get.width,
-                    height: Get.height * 0.07,
-                    child: Row(
+        child: OrientationBuilder(
+          builder: (context, orientation) {
+            if(orientation == Orientation.portrait){
+              return Container(
+                width: Get.width,
+                height: Get.height,
+                padding: EdgeInsets.symmetric(
+                  horizontal: Get.width * 0.025,
+                  vertical: Get.height * 0.02,
+                ),
+                child: Obx(
+                      () {
+                    FriendModel friendModel = controller.friendModel.value;
+                    return Column(
                       children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: Get.width * 0.03,
-                          ),
-                          child: const Icon(
-                            Icons.email,
-                          ),
+                        ///
+                        /// image
+                        Image.network(
+                          friendModel.picture!.large ?? '',
+                          width: Get.width,
+                          height: Get.height * 0.6,
+                          fit: BoxFit.cover,
                         ),
+
+                        SizedBox(
+                          height: Get.height * 0.01,
+                        ),
+
+                        ///
+                        /// address
                         Text(
-                          friendModel.email ?? '',
+                          ('${friendModel.location?.city}, ' ?? '') + ('${friendModel.location?.state}, ' ?? '') + ('${friendModel.location?.country}, ' ?? ''),
                           style: TextStyle(
                             fontSize: Get.textScaleFactor * 20,
                           ),
                         ),
-                      ],
-                    ),
-                  ),
 
-                  SizedBox(
-                    height: Get.height * 0.02,
-                  ),
+                        SizedBox(
+                          height: Get.height * 0.02,
+                        ),
 
-                  ///
-                  /// phone number
-                  Container(
-                    color: Colors.grey[400],
-                    width: Get.width,
-                    height: Get.height * 0.07,
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: Get.width * 0.03,
-                          ),
-                          child: const Icon(
-                            Icons.phone,
+                        ///
+                        /// email
+                        GestureDetector(
+                          onTap: () async{
+                            final Uri emailUri = Uri(
+                              scheme: 'mailto',
+                              path: friendModel.email,
+                            );
+
+                            try{
+                              await launchUrl(emailUri);
+                            } catch(error){
+                              if (kDebugMode) {
+                                print('cannot launch uri: $error');
+                              }
+                            }
+                          },
+                          child: Container(
+                            color: Colors.grey[400],
+                            width: Get.width,
+                            height: Get.height * 0.07,
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: Get.width * 0.03,
+                                  ),
+                                  child: const Icon(
+                                    Icons.email,
+                                  ),
+                                ),
+                                Text(
+                                  friendModel.email ?? '',
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                        Text(
-                          friendModel.phone ?? '',
-                          style: TextStyle(
-                            fontSize: Get.textScaleFactor * 20,
+
+                        SizedBox(
+                          height: Get.height * 0.02,
+                        ),
+
+                        ///
+                        /// phone number
+                        Container(
+                          color: Colors.grey[400],
+                          width: Get.width,
+                          height: Get.height * 0.07,
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: Get.width * 0.03,
+                                ),
+                                child: const Icon(
+                                  Icons.phone,
+                                ),
+                              ),
+                              Text(
+                                friendModel.phone ?? '',
+                              ),
+                            ],
                           ),
                         ),
                       ],
-                    ),
-                  ),
-                ],
+                    );
+                  },
+                ),
               );
-            },
-          ),
+            } else{
+              return Container(
+                width: Get.width,
+                height: Get.height,
+                padding: EdgeInsets.symmetric(
+                  horizontal: Get.width * 0.025,
+                  vertical: Get.height * 0.02,
+                ),
+                child: Obx(
+                      () {
+                    FriendModel friendModel = controller.friendModel.value;
+                    return Column(
+                      children: [
+                        ///
+                        /// image
+                        Image.network(
+                          friendModel.picture!.large ?? '',
+                          width: Get.width,
+                          height: Get.height * 0.6,
+                          fit: BoxFit.cover,
+                        ),
+
+                        SizedBox(
+                          height: Get.height * 0.01,
+                        ),
+
+                        ///
+                        /// address
+                        Text(
+                          ('${friendModel.location?.city}, ' ?? '') + ('${friendModel.location?.state}, ' ?? '') + ('${friendModel.location?.country}, ' ?? ''),
+                          style: TextStyle(
+                            fontSize: Get.textScaleFactor * 20,
+                          ),
+                        ),
+
+                        SizedBox(
+                          height: Get.height * 0.02,
+                        ),
+
+                        ///
+                        /// email
+                        GestureDetector(
+                          onTap: () async{
+                            final Uri emailUri = Uri(
+                              scheme: 'mailto',
+                              path: friendModel.email,
+                            );
+
+                            try{
+                              await launchUrl(emailUri);
+                            } catch(error){
+                              if (kDebugMode) {
+                                print('cannot launch uri: $error');
+                              }
+                            }
+                          },
+                          child: Container(
+                            color: Colors.grey[400],
+                            width: Get.width,
+                            height: Get.height * 0.07,
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: Get.width * 0.03,
+                                  ),
+                                  child: const Icon(
+                                    Icons.email,
+                                  ),
+                                ),
+                                Text(
+                                  friendModel.email ?? '',
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(
+                          height: Get.height * 0.02,
+                        ),
+
+                        ///
+                        /// phone number
+                        Container(
+                          color: Colors.grey[400],
+                          width: Get.width,
+                          height: Get.height * 0.07,
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: Get.width * 0.03,
+                                ),
+                                child: const Icon(
+                                  Icons.phone,
+                                ),
+                              ),
+                              Text(
+                                friendModel.phone ?? '',
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              );
+            }
+          }
         ),
       ),
     );
